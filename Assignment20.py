e01c9d6125e143bda218970e88e46fd545dc9244
YOGINI PAWAR <83333272+yoginipawar@users.noreply.github.com>
2024-07-15 16:02:05 +0530

    Update Assignment20.py

--- Assignment20.py
+++ Assignment20.py
@@ -9,4 +9,10 @@ for key, value in dict_num.items():
     print(key, "    ", value)
 
 #OUTPUT
-#Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+key  value
+1      10
+2      20
+3      30
+4      40
+5      50
+6      60
