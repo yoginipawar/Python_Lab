5bdb7646c802f56b1722717024a9e67d24950d64
YOGINI PAWAR <83333272+yoginipawar@users.noreply.github.com>
2024-07-15 15:56:23 +0530

    Add files via upload

---
+++ Assignment20.py
@@ -0,0 +1,9 @@
+#Write a Python program to get the key, value and item in a dictionary.
+
+# Input dictionary
+dict_num = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+# Printing the keys and values
+print("key", " value")
+for key, value in dict_num.items():
+    print(key, "    ", value)
